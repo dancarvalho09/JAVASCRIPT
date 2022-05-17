8ce5d839aaf6569cf503dce775b26b3fe90eff51
Danúbia Carvalho <74597179+dancarvalho09@users.noreply.github.com>
2022-05-17 18:40:51 -0300

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+//console.log('O console está funcionando!')
+
+var velocidade = 30
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+
+if(velocidade > 60) {
+    console.log('Velocidade acima do permitido. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
