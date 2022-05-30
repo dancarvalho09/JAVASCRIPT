0f53bde8c27a3232d67345eed0dacfef3ff05c37
Danúbia Carvalho <74597179+dancarvalho09@users.noreply.github.com>
2022-05-30 16:46:59 -0300

    Create ambiente.js
    Criado ambiente de teste para ver a funcionalidade 'while' e 'do ... while'.

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+
+var contador = 1
+
+while (contador <= 6) {
+    console.log(`Enquanto ${contador}`)
+    contador++ // contador = contador + 1
+}
+
+
+console.log('-----------------------------------------')
+
+var contador = 1
+do{
+    console.log(`Faça ${contador}`)
+    contador++
+} while(contador <= 6)
