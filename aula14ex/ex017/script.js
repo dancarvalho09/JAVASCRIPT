9db66737d7ee61f8ab066cb90b37375584204628
Danúbia Carvalho <74597179+dancarvalho09@users.noreply.github.com>
2022-05-31 18:49:11 -0300

    Update script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,25 @@
-window.alert('Olá!')
+
+function tabuada () {
+    let num = document.getElementById('txtnum')
+    let tab = document.getElementById('tab')
+    
+    
+
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número válido!')
+
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            tab.appendChild(item)
+            c++
+        }
+        
+    }
+}
